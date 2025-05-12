fd1cc4414266b8f51224b51e0f6e3b0e8267f8bd
Hamza Anjum <hamzaanjum650@gmail.com>
2025-05-12 06:59:25 -0700

    QR Code Generator
    A simple and efficient QR Code Generator built using Python. This tool allows users to convert text or URLs into scannable QR codes, which can be saved as image files.
    
    🚀 Features
    Generate QR codes for any text, URL, or data.
    
    Save QR codes as .png image files.
    
    Lightweight and easy to use.
    
    Uses the qrcode and Pillow libraries.

--- QR_Code_Generator.py
+++ QR_Code_Generator.py
@@ -1,16 +1,16 @@
 # Method no 1 : Simple qr code without specifing any color or border of QR.
-import qrcode as qr
-import qrcode.console_scripts
-import qrcode.constants
-image = qr.make("https://chatgpt.com/") # Link or Sentence which QR has to be made
-image.save("Chatgpt.png")
+# import qrcode as qr
+# import qrcode.console_scripts
+# import qrcode.constants
+# image = qr.make("https://chatgpt.com/") # Link or Sentence which QR has to be made
+# image.save("Chatgpt.png")
 
 # # Method no 2 : Customize qr code with customize any color or border of QR.
 import qrcode
 from PIL import Image
 qr = qrcode.QRCode(
     version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=20, border=1,)
-qr.add_data("https://chatgpt.com/")
+qr.add_data("Hamza is a  boy")
 qr.make(fit=True)
-img = qr.make_image(fill_color="pink", back_color="white")
+img = qr.make_image(fill_color="red", back_color="white")
 img.save("Chatgpt.png")
